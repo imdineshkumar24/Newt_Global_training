8021267bb20a9323bd36ca58f5001e8a6428097e
imdineshkumar24 <imdineshkumar142@gmail.com>
2025-04-02 16:58:21 +0530

    Files added in 02042025

---
+++ Java/Java-full-course/Encapsulation/assignments.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+import java.util.*;
+public class assignments {
+    public static void main(String[] args) {
+        Person person = new Person("John", 25, "Male");
+        person.setName("Mike");
+        person.setAge(200);
+        System.out.println(person);
+
+    }
+}
